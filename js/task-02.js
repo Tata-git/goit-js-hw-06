23a2409dbb0e6031d9ce82167ff0b6997b0e3cd9
Tata-Link <83451272+Tata-git@users.noreply.github.com>
2022-04-20 15:05:41 +0300

    task-01-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+console.log(ingredientsEl);
+
+const listElements = ingredients.map((ingredient) => {
+  const list = document.createElement("li");
+  console.log(list);
+
+  list.textContent = ingredient;
+  list.classList.add("item");
+
+  return list;
+});
+
+ingredientsEl.append(...listElements);
+console.log(ingredientsEl);
